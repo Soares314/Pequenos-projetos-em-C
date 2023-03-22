a947c5e319f7add17c3623c0ac6c7813d23a6594
Soares314 <105524605+Soares314@users.noreply.github.com>
2023-03-22 11:01:52 -0300

    Add files via upload

---
+++ cu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+
+{
+    int test = 127;
+    char strin = '75';
+    printf("Conhecimento: %c\n %x\n", test, test);
+    system("PAUSE");
+    return 0;
+}
