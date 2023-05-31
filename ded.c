e9673c4e89e8721003485cacb1cb61641affc99c
Soares314 <105524605+Soares314@users.noreply.github.com>
2023-05-31 11:44:56 -0300

    Add files via upload

---
+++ ded.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+    char Z[] = {'A', 0},
+         U[] = {7, 3},
+         D[] = {2, 5},
+         T[] = {'D', 3},
+         Q[] = {'C', 'A'},
+         C[] = {'F', 'B'};
+    char crip[] = {Z, U, D, T, Q, C};
+    return 0;
+}
+int soares(char cri[]){
+
+
+}
